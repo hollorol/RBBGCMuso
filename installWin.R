2de9c649c14254abdfafc77dc49705b9e783bdec
hollorol <hollorol@gmail.com>
2018-11-19 20:29:52 +0100

    Adding installation script for windows

---
+++ installWin.R
@@ -0,0 +1,5 @@
+basePackages <- c("dplyr","digest","ggplot2","shiny","latex2expr",
+                  "magrittr","tibble","tidyr","installr")
+install.packages(basePackages)
+install.packages("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_0.5.0.0-0.zip", repos = NULL, type = "win.binary")
+
