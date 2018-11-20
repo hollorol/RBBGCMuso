34c2089c4bbdb17113f6b4e12d2e43f2aec3a067
Roland Hollós <hollorol@meteor22.elte.hu>
2018-11-20 01:02:21 +0100

    Making examples

---
+++ RBBGCMuso/R/musoExamples.R
@@ -0,0 +1,58 @@
+#'copyMusoExampleTo
+#'
+#'With this function you can copy RBBGCMuso example library whereever you want
+#'
+#'@param example The name of the example file, if it is NULL tcl/tk menu will pop up to select.
+#'@return List of two matrices: A and B matrix for musoRandomizer
+#'@import stats
+#'@import magrittr
+#'@import dplyr
+#'@export
+
+copyMusoExamleTo <- function(example = NULL, destination = NULL){
+    WindowsP <- Sys.info()[1] == "Windows"
+
+    chooseExample <- function(){
+            choiceWin <- tcltk::tktoplevel()
+            tcltk::tclRequire("BWidget")
+            tcltk::tktitle(choiceWin) <- "Choose an example!"
+            tcltk::tcl("wm","geometry",choiceWin,"200x50")
+            choiceValues <-  basename(list.dirs(system.file("examples","",package = "RBBGCMuso"),recursive = FALSE))
+            choices <- tcltk::tkwidget(choiceWin,"ComboBox",
+                                       editable = FALSE, values = choiceValues,
+                                       textvariable = tclVar(choiceValues[1]))
+            tkpack(choices)
+            choiceValue <- NA
+            closeSelection <- tcltk::tkwidget(choiceWin,"button",text ="Select", command =function (){
+                choiceValue <<- tcltk::tclvalue(tcltk::tcl(choices,"get"))
+                tkdestroy(choiceWin)
+            })
+            
+            tcltk::tkpack(closeSelection)
+            while(as.numeric(tcltk::tclvalue(tcltk::tcl("winfo","exists",choiceWin)))){
+                
+            }
+            return(choiceValue)
+    }
+
+
+    
+    if(is.null(example)){
+        cExample<-chooseExample()       
+    }
+    
+    if(is.null(destination)){
+        destination<-tcltk::tk_choose.dir(getwd(), "Choose folder to copy the examples!")
+    }
+    return(c(cExample,destination))
+     
+   
+}
+## choiceWin <- tcltk::tktoplevel()
+## tclRequire("BWidget")
+## 
+## choiceValues <-  basename(list.dirs("~/Documents/projects",recursive = FALSE))
+## choices <- tkwidget(choiceWin,"ComboBox", editable = FALSE, values = choiceValues, textvariable = tclVar(choiceValues[1]))
+## tkpack(choices)
+## filename<- tclvalue(tcl(choices,"get"))
+## filename
