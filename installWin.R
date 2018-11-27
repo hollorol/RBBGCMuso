52b0f0553b77c1f6d6b98f445fedb2d1af518792
hollorol <hollorol@gmail.com>
2018-11-27 05:14:32 +0100

    upload binary package

--- installWin.R
+++ installWin.R
@@ -3,3 +3,4 @@ basePackages <- c("dplyr","digest","ggplot2","shiny","latex2expr",
 install.packages(basePackages)
 install.packages("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_0.5.0.0-0.zip", repos = NULL, type = "win.binary")
 
+copyMu
