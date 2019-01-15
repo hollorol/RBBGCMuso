4efed52ad8669bb07000f3def8ed3d7c17cea1d6
hollorol <hollorol@gmail.com>
2019-01-15 21:10:45 +0100

    Version change and windows binary package

--- installWin.R
+++ installWin.R
@@ -1,4 +1,4 @@
 basePackages <- c("dplyr","digest","ggplot2","shiny","latex2expr",
                   "magrittr","tibble","tidyr")
 install.packages(basePackages)
-install.packages("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_0.6.0.0-2.zip", repos = NULL, type = "win.binary")
+install.packages("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_0.6.0.1-0.zip", repos = NULL, type = "win.binary")
