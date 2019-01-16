9c919c09ecb4b95d5951bb9e8f3691651393d43c
hollorol <hollorol@gmail.com>
2019-01-16 01:34:07 +0100

    More advanced automatic Windows installer

--- installWin.R
+++ installWin.R
@@ -1,5 +1,4 @@
-basePackages <- c("dplyr","digest","ggplot2","shiny","latex2expr",
-                  "magrittr","tibble","tidyr","limSolve")
-install.packages(basePackages)
-install.packages("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_0.6.0.1-0.zip", repos = NULL, type = "win.binary")
-rm(basePackages)
+install.packages(c("dplyr","digest","ggplot2","shiny","latex2expr",
+                   "magrittr","tibble","tidyr","limSolve"))
+.RBBGCMusoVersion <- gsub("Version\\:\\s","",grep("Version",readLines("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso/DESCRIPTION"),value=TRUE))
+install.packages(paste0("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_",.RBBGCMusoVersion,".zip"), repos = NULL, type = "win.binary")
