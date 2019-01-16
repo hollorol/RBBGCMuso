aaf8828c9a96672f80d0e4e43e084b90176e5419
hollorol <hollorol@gmail.com>
2019-01-16 01:42:58 +0100

    Adding rmarkdown to the installation process

--- installWin.R
+++ installWin.R
@@ -1,4 +1,4 @@
 install.packages(c("dplyr","digest","ggplot2","shiny","latex2expr",
-                   "magrittr","tibble","tidyr","limSolve"))
+                   "magrittr","tibble","tidyr","limSolve","rmarkdown"))
 .RBBGCMusoVersion <- gsub("Version\\:\\s","",grep("Version",readLines("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso/DESCRIPTION"),value=TRUE))
 install.packages(paste0("https://github.com/hollorol/RBBGCMuso/raw/master/RBBGCMuso_",.RBBGCMusoVersion,".zip"), repos = NULL, type = "win.binary")
