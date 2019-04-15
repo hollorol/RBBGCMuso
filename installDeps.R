2c9b268402d769382845a5edd48df814129c6091
Roland Hollós <hollorol@gmail.com>
2019-04-15 11:57:55 +0200

    Modify the installation scripts!

--- installDeps.R
+++ installDeps.R
@@ -1,8 +1,18 @@
 (function(){
   packagesToInstall <- c("shiny","shinyjs","plotly","promises","future","data.table","rhandsontable")
-  sapply(packagesToInstall, function(pkgs){
+  installedp<- sapply(packagesToInstall, function(pkgs){
     if(!is.element(pkgs,installed.packages()[,1])){
       install.packages(pkgs)
+      if(!is.element(pkgs,installed.packages([,1]))){
+        return(FALSE)
+      } else {
+        return(TRUE)
+      }
+    } else {
+      return(TRUE)
     }
   })
+  if(any(!installedp)){
+   stop("The installation process was not successful. Please try rerun the installation!") 
+  }
 })()
