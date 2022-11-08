aac846d5af896582e2dc2b434f3cfb554cf2a778
Roland Hollós <hollorol@gmail.com>
2022-11-08 09:23:19 +0100

    BUG for one core machines

--- RBBGCMuso/R/calibrateMuso.R
+++ RBBGCMuso/R/calibrateMuso.R
@@ -113,12 +113,16 @@ calibrateMuso <- function(measuredData, parameters =read.csv("parameters.csv", s
     #  \____\___/|_| |_| |_|_.__/|_|_| |_|\___|
     resultFiles <- list.files(pattern="preservedCalib.*csv$",recursive=TRUE)
     res0 <- read.csv(grep("thread_1/",resultFiles, value=TRUE),stringsAsFactors=FALSE)
-    resultFilesSans0 <- grep("thread_1/", resultFiles, value=TRUE, invert=TRUE)
-    # results <- do.call(rbind,lapply(resultFilesSans0, function(f){read.csv(f, stringsAsFactors=FALSE)}))
-    resultsSans0 <- lapply(resultFilesSans0, function(f){read.csv(f, stringsAsFactors=FALSE, header=FALSE)})
-    resultsSans0 <- do.call(rbind,resultsSans0)
-    colnames(resultsSans0) <- colnames(res0)
-    results <- (rbind(res0,resultsSans0))
+    if(numCores==1){
+        result <- res0
+    } else {
+        resultFilesSans0 <- grep("thread_1/", resultFiles, value=TRUE, invert=TRUE)
+        # results <- do.call(rbind,lapply(resultFilesSans0, function(f){read.csv(f, stringsAsFactors=FALSE)}))
+        resultsSans0 <- lapply(resultFilesSans0, function(f){read.csv(f, stringsAsFactors=FALSE, header=FALSE)})
+        resultsSans0 <- do.call(rbind,resultsSans0)
+        colnames(resultsSans0) <- colnames(res0)
+        results <- (rbind(res0,resultsSans0))
+    }
 
     switch(method,
            "GLUE"={  
