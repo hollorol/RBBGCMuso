3a01e32a7f07ad9306ed18c11484b79ea31b3e5c
Zrinyi Nándor <nandor.zrinyi@gmail.com>
2025-04-09 18:29:07 +0200

    version numbering bruh

--- RBBGCMuso/R/tuner.R
+++ RBBGCMuso/R/tuner.R
@@ -5247,7 +5247,7 @@ current_logs <- runLogs()
                 id = "info_overlay",
                 style = "display:none; position:absolute; top:44px; left:0; width:100%; background:#f9f9f9; border:1px solid #ccc; padding:10px; z-index:1050;",
                 tags$p(div(HTML("
-                    <p><strong>Version 2.18.2</strong></p>
+                    <p><strong>Version 2.19.0</strong></p>
                     <p>Current known bugs/problems:</p>
                     <ul>
                         <li>Auto-calculation for allocation can make the sliders oscillate between two values due to accuracy contraint (if it wants to calulate using 3 or more sliders). If that happens, turn off auto-calc if they can't find values within a few seconds.</li>
